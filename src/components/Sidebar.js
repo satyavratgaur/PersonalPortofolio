2d803187e0921277af421fedeb4a9842b5ff33a5
Satyavrat <gaur.satyavrat1995@gmail.com>
2020-07-08 02:40:01 -0700

    Adding Side Cards

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import theme from '../styles/theme';
-import CardMedia from '@material-ui/core/CardMedia';
 import profilePhoto from '../assets/universal_photo.jpeg';
 import { hexToRgbA } from '../helpers/helpers';
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 
 const primaryColor = theme.palette.primary.main;
 const initialGray = theme.palette.gray.main;
+
 const SidebarContainer = styled(Grid)`
   align-content: center;
   text-align: center;
@@ -30,16 +31,41 @@ const Link = styled.a`
   }
 `;
 
-const SideCard = styled.div``;
+const SideCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* border: 2px solid red; */
+  border-radius: 12px;
+`;
+
+const SideCardTop = styled.div`
+  display: flex;
+  background-color: grey;
+  flex: 1;
+  padding: 0px 12px;
+  border-radius: 12px;
+`;
+
+const Quote = styled.div`
+  margin: 12px 0px;
+  padding: 12px;
+  text-align: left;
+  border-left: 4px solid ${primaryColor};
+`;
+
+const SideCardBottom = styled.div`
+  display: flex;
+  flex: 2;
+  flex-direction: ${(props) => (props.flexdir === 'column' ? 'column' : 'row')};
+  padding: 12px 12px;
+  border-radius: 12px;
+`;
 
 const Sidebar = ({ sidebarContent }) => {
   return (
     <SidebarContainer item container direction='column'>
       <SidebarImage src={profilePhoto} alt='UniversalStudios' />
-
-      <div>
-        <h2>Satyavrat Gaur</h2>
-      </div>
+      <h2>Satyavrat Gaur</h2>
 
       {/* Social Media Information */}
       {sidebarContent.social.map((item) => {
@@ -47,7 +73,7 @@ const Sidebar = ({ sidebarContent }) => {
           display: flex;
           align-items: center;
           color: ${initialGray};
-          padding: 12px 24px;
+          padding: 0px 24px;
           &:hover {
             color: ${item.socialMediabg};
             background: ${hexToRgbA(item.socialMediabg, 0.15)};
@@ -78,6 +104,39 @@ const Sidebar = ({ sidebarContent }) => {
           </SocialMediaContainer>
         );
       })}
+
+      {/* Side Card */}
+      <SideCard>
+        <SideCardTop>
+          <h4>Education</h4>
+          <p>
+            Exercitation proident nisi minim dolor cillum labore magna cupidatat
+            aute exercitation adipisicing sint. Nisi anim nostrud incididunt
+          </p>
+        </SideCardTop>
+        <Quote>
+          <p>
+            <i>
+              "Exercitation proident nisi minim dolor cillum labore magna
+              cupidatat aute exercitation adipisicing sint. Nisi anim nostrud
+              incididunt labore aute ex et aliquip. Id eiusmod duis duis enim
+              ullamco occaecat. Anim nulla excepteur exercitation minim veniam
+              excepteur mollit duis. Ex et nulla minim ex quis cupidatat dolor
+              laborum excepteur. Minim do nulla commodo occaecat labore dolor
+              sit id sit nostrud incididunt occaecat non. Exercitation
+              incididunt ex est veniam minim aliquip."{' '}
+            </i>
+            <br />
+            <br />
+            <i> - Da Vinci</i>
+          </p>
+        </Quote>
+        <Divider />
+        <SideCardBottom flexdir='column'>
+          <p>"If you want to write more stuff"</p>
+          <p>"If you want to write more stuff"</p>
+        </SideCardBottom>
+      </SideCard>
     </SidebarContainer>
   );
 };
